939b38d6768079d40650e33fa630fd18a2a41d34
Jennifer Lai <jenniferlai.81@gmail.com>
2019-04-06 00:02:35 -0700

    setup axios call to get random words for the output

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,9 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min';
 import React, {Component} from 'react';
 import '../assets/css/app.scss';
 import logo from '../assets/images/logo.svg';
+import GenerateData from './output/generate_data';
 
 class App extends Component {
 
@@ -9,12 +12,12 @@ class App extends Component {
             <div>
                 <div className="app">
                     <img src={logo} className="logo rotate"/>
-                    <h1>Welcome To React</h1>
+                    <GenerateData />
                 </div>
             </div>
         );
     }
-} 
+}
 
 
 
--- src/components/output/generate_data.js
+++ src/components/output/generate_data.js
@@ -1,7 +1,34 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class GenerateData extends Component {
+    state = {
 
+    }
+
+    componentDidMount(){
+        this.getRandomWords(2)
+    }
+
+    getRandomWords(numOfWords){
+        const resp = axios.get('https://random-word-api.herokuapp.com/word', {
+            params: {
+                key: 'LXUJXE7X',
+                number: numOfWords
+            }
+        }).then((resp) => {
+            console.log('response', resp)
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>HERE IS YOUR DATA</h1>
+
+            </div>
+        );
+    }
 }
 
 export default GenerateData;
