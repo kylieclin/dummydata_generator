cf25fa8a7131df5c6fb19563c265467d4a535be2
Jennifer Lai <jenniferlai.81@gmail.com>
2019-04-07 00:57:50 -0700

    random words axios call working

--- src/components/output/generate_data.js
+++ src/components/output/generate_data.js
@@ -3,27 +3,35 @@ import axios from 'axios';
 
 class GenerateData extends Component {
     state = {
-
+        words: [],
+        numOfWords: 2
     }
 
     componentDidMount(){
-        this.getRandomWords(2)
+        //needs to be dynamic
+        this.getRandomWords('name')
     }
 
-    getRandomWords(numOfWords){
-        const getKey = axios.get('//https://yacdn.org/proxy/https://random-word-api.herokuapp.com/key');
-
-        const resp = axios.get('https://random-word-api.herokuapp.com/word', {
+    getRandomWords(fieldWord){
+        const resp = axios.get('https://api.datamuse.com/words', {
             params: {
-                key: key,
-                number: numOfWords
+                rel_gen: fieldWord
             }
         }).then((resp) => {
-            console.log('response', resp)
-        });
+            this.setState({
+                words: resp.data
+            });
+        })
     }
 
     render(){
+        console.log('state',this.state)
+        const {words, numOfWords} = this.state;
+        console.log('words', words)
+        const inputValue = ''
+        for(var i = 0; i < numOfWords; i++){
+            //inputValue +=
+        }
         return(
             <div>
                 <h1>HERE IS YOUR DATA</h1>
