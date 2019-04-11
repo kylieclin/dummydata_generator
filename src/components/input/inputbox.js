f0d6cdb518ccf099dee36a0097d1a4963ca8ea19
Kylie Lin <kylieclin@gmail.com>
2019-04-10 22:09:22 -0700

    added copy btn for copy results copy result function work add detail fields for input detail on str and num need to work on clean the input and delete rows

--- src/components/input/inputbox.js
+++ src/components/input/inputbox.js
@@ -6,11 +6,15 @@ class InputBox extends Component {
 
         this.state= {
             field: '',
-            option:''
+            option:'',
+            max: '1',
+            min: '0',
+            input_detail: null
         };
 
         this.handleInput = this.handleInput.bind(this);
         this.handleSelect = this.handleSelect.bind(this);
+        this.handleInputDetail = this.handleInputDetail.bind(this);
 
     }
     handleInput(event){
@@ -19,8 +23,51 @@ class InputBox extends Component {
         })
     }
     handleSelect(event){
+        let detail = null;
+        switch(event.target.value){
+            case 'string':
+            detail= this.stringOption();
+            break;
+            case 'number':
+            detail=this.numberRange();
+            break;
+        }
         this.setState({
-            option: event.target.value
+            option: event.target.value,
+            input_detail: detail
+        })
+    }
+    handleInputDetail(event){
+        this.setState({
+            max: event.target.value
+        })
+    }
+    stringOption= ()=>{
+        return(
+            <div className="col s2 m1">
+                <label htmlFor="strlen">Length</label> 
+                <input id="strlen" className="strlen" type="number" onChange={this.handleInputDetail} min="1" max="200"/>
+            </div>
+        )
+    }
+    numberRange= ()=>{
+        return(
+            <Fragment>
+                <div className="col s1 m1">
+                <label htmlFor="minnum">From</label> 
+                <input id="minnum" type="number" onChange={this.handleMin}/>
+                </div>
+                <div className="col s1 m1">
+                <label htmlFor="maxnum">To</label> 
+                <input id="maxnum" type="number" onChange={this.handleInputDetail}/>
+                </div>
+            </Fragment>
+        )
+    }
+    handleMin= (event)=>{
+        console.log(event.target.value);
+        this.setState({
+            min: event.target.value
         })
     }
     componentDidMount(){
@@ -29,7 +76,7 @@ class InputBox extends Component {
     render(){
         return (
             <Fragment>
-                <div className="field-input col s8 m6">
+                <div className="field-input col s6 m6">
                     <form className=""action="">
                         <label htmlFor="field">Field</label>
                         <input id="field" name="field" className="field" type="text" value={this.field} onChange={this.handleInput} placeholder="Enter Field"/>
@@ -37,8 +84,8 @@ class InputBox extends Component {
                 </div>
                 <div className="type-select col s4 m2">
                     <label>Select Data Type
-                    <select  defaultValue="" onChange={this.handleSelect} ref={(element)=>{this.formSelect = element}}>
-                        <option value="" disabled>Select</option>
+                    <select  defaultValue="default" onChange={this.handleSelect} ref={(element)=>{this.formSelect = element}}>
+                        <option value="default" disabled>Select</option>
                         <option value="string">String</option>
                         <option value="number">Number</option>
                         <option value="object">Object</option>
@@ -47,6 +94,9 @@ class InputBox extends Component {
                     </select>
                     </label>
                 </div>
+
+                    {this.state.input_detail}
+
             </Fragment>
         )
     }
--- src/components/output/modal.js
+++ src/components/output/modal.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 
 class OutputModal extends Component{
+    state={
+        message: ''
+    }
     componentDidMount(){
         const option ={
                 startingTop: '20%'
@@ -8,15 +11,29 @@ class OutputModal extends Component{
         M.Modal.init(this.modal, option).open();
 
     }
+    copyJson= ()=>{
+        this.modalContent.select();
+        document.execCommand("copy");
+        this.setState({
+            message: 'copied!'
+        })
+    }
     render(){
-        debugger;
+
         return(
         <div id="modal1" className="modal" ref={element=>{this.modal = element}}>
-            <div className="modal-content">
-                <pre>{this.props.output}</pre>
+            <div id="modalContent" className="modal-content" ref="modalContent">
+                <label>Your Dummy Data is here :)</label>
+                <textarea className="materialize-textarea" ref={e =>{this.modalContent = e}} value={this.props.output} readOnly>
+                </textarea>
+                <div>{this.state.message}</div>
             </div>
             <div className="modal-footer">
-              <a href="#!" className="modal-close waves-effect waves-green btn-flat">Agree</a>
+                
+                <button className="waves-effect waves-green btn-flat" onClick={this.copyJson}>
+                    <i className="material-icons">content_copy</i>
+                </button>
+                <a href="#!" className="modal-close waves-effect waves-green btn-flat">Close</a>
             </div>
         </div>
         )
